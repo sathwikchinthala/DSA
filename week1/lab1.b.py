d93c7afebd3f9a3a31ea0ab056c307a8083b2f6f
sathwikchinthala <sathwikchinthala1@gmail.com>
2026-08-06 00:10:22 +0530

    Add files via upload

---
+++ week1/lab1.b.py
@@ -0,0 +1,10 @@
+def compound_interest(p, n):
+    if n == 0:
+        return 1
+    else:
+        return p * compound_interest(p, n - 1)
+
+p = int(input("Enter the principal growth: "))
+n = int(input("Enter the number of years: "))
+
+print(compound_interest(p, n))
