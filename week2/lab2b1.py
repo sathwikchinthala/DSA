536b6bef8cf46196ceddb884f76054566880897f
sathwikchinthala <sathwikchinthala1@gmail.com>
2026-08-12 23:12:57 +0530

    Add files via upload

---
+++ week2/lab2b1.py
@@ -0,0 +1,26 @@
+def binary_search(arr, k):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if k == arr[mid]:
+            return mid
+        elif k < arr[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return -1
+
+
+n = int(input("Enter the no of elements: "))
+arr = []
+
+for i in range(n):
+    arr.append(int(input("Enter the element: ")))
+
+k = int(input("Enter the search element: "))
+
+print(binary_search(arr, k))
